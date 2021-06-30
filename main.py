6e636ddf2d6c8b3106e18392091fa3048c8acec9
Antoine <antoine.caputo1@viacesi.fr>
2021-06-30 10:23:24 +0200

    Pleins de modifications (dict_lieu, génération aléatoire nb véhicules) + début de génération d'une tournée

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
+import math
+
+import numpy
 import random
 from enum import Enum
 
@@ -27,16 +30,16 @@ class TypeColis(Enum):
     LETTRE = "lettre"
 
 
-def fctGénérerColis(_tb_lieux, _nb_colis):
+def fctGénérerColis(_dic_lieux, _nb_colis):
     tb_colis = []
 
     for i in range(_nb_colis):
         type_colis = random.choice(list(TypeColis))
 
         # do while
-        pt_livraison = _tb_lieux[random.randint(0, len(tb_lieux) - 1)]
+        pt_livraison = _dic_lieux(random.choice(list(_dic_lieux.keys())))
         while pt_livraison.type_lieu is not TypeLieu.LIVRAISON:
-            pt_livraison = _tb_lieux[random.randint(0, len(tb_lieux) - 1)]
+            pt_livraison = _dic_lieux(random.choice(list(_dic_lieux.keys())))
 
         tb_colis.append(Colis(type_colis, pt_livraison.nom_lieu))
 
@@ -93,6 +96,29 @@ class Route:
         self.type_route = _type_route
         self.longueur = _longueur
 
+    def fctCalculerTempsTrajet(self, _datetime, _type_véhicule):
+
+        if str(_datetime) not in self.dic_info_trafic:
+            return None
+
+        vitesse = 0
+        if self.type_route is TypeRoute.AUTOROUTE:
+            if _type_véhicule is (TypeVéhicule.MOTO or TypeVéhicule.VOITURE):
+                vitesse = 130
+            elif _type_véhicule is TypeVéhicule.CAMION:
+                vitesse = 100
+        elif self.type_route is TypeRoute.NATIONALE:
+            vitesse = 80
+        elif self.type_route is TypeRoute.DEPARTEMENTALE:
+            vitesse = 80
+        elif self.type_route is TypeRoute.COMMUNALE:
+            vitesse = 80
+
+        # t = v / d * (ln trafic +1)
+        temps_trajet = vitesse / (self.longueur * (math.log(self.dic_info_trafic[str(_datetime)]) + 1))
+
+        return temps_trajet
+
     """
     Génére des données de trafic d'un axe routier
 
@@ -135,17 +161,20 @@ class Route:
             période_actuelle = 0
 
             """
-            niv_trafic = random.randint(0, nb_véhicules)
+            niv_trafic = numpy.random.randint(0, nb_véhicules)
             nb_véhicules = 0
             if niv_trafic == 1:
-                nb_véhicules = random.randint(0, 2)
+                nb_véhicules = numpy.random.randint(0, 2)
             elif niv_trafic == 2:
-                nb_véhicules = random.randint(3, 6)
+                nb_véhicules = numpy.random.randint(3, 6)
             elif niv_trafic == 3:
-                nb_véhicules = random.randint(6, 10)
+                nb_véhicules = numpy.random.randint(6, 10)
             """
+            # TODO : random --> toujours la même série de nombre
+            random.seed()
 
-            fct_trafic = random.randint(0, 1)
+            # fct_trafic = random.randint(0, 1)
+            nb_véhicules = random.randint(0, 10)
 
             while période_actuelle < _nb_périodes:
 
@@ -153,22 +182,26 @@ class Route:
 
                 """
                 if niv_trafic == 1:
-                    nb_véhicules = random.randint(0, 20)
+                    nb_véhicules = numpy.random.randint(0, 20)
                 elif niv_trafic == 2:
-                    nb_véhicules = random.randint(21, 50)
+                    nb_véhicules = numpy.random.randint(21, 50)
                 elif niv_trafic == 3:
-                    nb_véhicules = random.randint(51, 100)
+                    nb_véhicules = numpy.random.randint(51, 100)
                 """
+                random.seed()
+                nb_véhicules += random.randint(-10, 10)
 
-                nb_véhicules = 0
+                if nb_véhicules < 0:
+                    nb_véhicules = 0
 
+                """
                 if fct_trafic == 0:
                     # 5x − x²
                     nb_véhicules = 5 * période_actuelle - période_actuelle ** 2
                 elif fct_trafic == 1:
                     # f(x)=3-(2+x^(3)-2 x^(2))
                     nb_véhicules = 3 - (2 + (période_actuelle ** 3) - 2 * période_actuelle ** 2)
-
+                """
                 # ■■■■■■■■■■ Date ■■■■■■■■■■
 
                 heure = int((période_actuelle + 1) / 60)
@@ -196,7 +229,7 @@ class Route:
                 if plage_horaire_actuelle == 'm' and période_actuelle == _nb_périodes:
                     plage_horaire_actuelle = 's'
                     période_actuelle = 0
-                    niv_trafic = random.randint(1, 3)
+                    niv_trafic = numpy.random.randint(1, 3)
 
             # Passer au jour suivant
             jour_actuel += 1
@@ -209,13 +242,12 @@ class TypeRoute(Enum):
     COMMUNALE = "communale"  # 50
 
 
-def fctListeVoisins(_tb_lieux):
-
+def fctListeVoisins(_dic_lieux):
     dic_voisins_lieu = {}
 
     # complexité polynomiale !!
 
-    for lieu in _tb_lieux:
+    for lieu in _dic_lieux.items():
 
         # initialisation dictionnaire des voisins du lieu actuel
         if lieu.nom_lieu not in dic_voisins_lieu:
@@ -225,7 +257,7 @@ def fctListeVoisins(_tb_lieux):
         for id_route in lieu.tb_id_routes:
 
             # comparaison avec d'autres lieux qui ont les mêmes routes
-            for voisin in _tb_lieux:
+            for voisin in _dic_lieux.items():
 
                 if lieu is voisin:
                     continue
@@ -281,34 +313,45 @@ def fctGénérerPlan(_nb_lieux):
     if _nb_lieux < 2:
         raise ValueError("Le nombre de lieux doit au minimum être 2")
 
-    tb_lieux = []
+    # tb_lieux = []
+    dic_lieux = {}
 
     # tb_routes = []
     id_route = 0
     dic_routes = {}
 
-    nb_heures_ouvert = random.randint(5, 9)
+    nb_heures_ouvert = numpy.random.randint(5, 9)
     # Création des points de livraison
     for i in range(_nb_lieux):
         # TODO : - A tester - heure début fin aléatoire
         lieu = Lieu(i, TypeLieu.LIVRAISON, 7, 7 + nb_heures_ouvert)
-        tb_lieux.append(lieu)
+        dic_lieux[i] = lieu
+        # tb_lieux.append(lieu)
 
     # Un lieu aléatoire devient le dépot
-    index_lieu_aléatoire = random.randint(0, len(tb_lieux) - 1)
-    tb_lieux[index_lieu_aléatoire].type_lieu = TypeLieu.DEPOT
-    tb_lieux[index_lieu_aléatoire].heure_début = 6
-    tb_lieux[index_lieu_aléatoire].heure_fin = 22
+    index_dépot = random.choice(list(dic_lieux.keys()))
+    dépot = dic_lieux[index_dépot]
+    dépot.type_lieu = TypeLieu.DEPOT
+    dépot.heure_début = 6
+    dépot.heure_fin = 22
+    dic_lieux[index_dépot] = dépot
+
+
+    # index_lieu_aléatoire = numpy.random.randint(0, len(tb_lieux) - 1)
+    # tb_lieux[index_lieu_aléatoire].type_lieu = TypeLieu.DEPOT
+    # tb_lieux[index_lieu_aléatoire].heure_début = 6
+    # tb_lieux[index_lieu_aléatoire].heure_fin = 22
+    # index_dépot = index_lieu_aléatoire
 
     # Génération des routes
-    for lieu in tb_lieux:
+    for nom_lieu in dic_lieux:
         print("\nTraitement d'un lieu")
 
         # Au moins une route s'il n'en existe pas encore
-        if len(lieu.tb_id_routes) == 0:
+        if len(dic_lieux[nom_lieu].tb_id_routes) == 0:
             pourcentage = 100
         else:
-            pourcentage = random.randint(0, 100)
+            pourcentage = numpy.random.randint(0, 100)
 
         while pourcentage >= 55:
             print("Générer une route (" + str(pourcentage) + ")")
@@ -316,7 +359,7 @@ def fctGénérerPlan(_nb_lieux):
             # type de route aléatoire
             type_route = random.choice(list(TypeRoute))
             # longueur aléatoire
-            longueur_route = random.randint(1, 10)
+            longueur_route = numpy.random.randint(1, 10)
 
             # création de la route
             route = Route(type_route, longueur_route)
@@ -325,23 +368,23 @@ def fctGénérerPlan(_nb_lieux):
             route.fctGénérerTrafic(1, 120)
 
             # voisin aléatoire, différent du lieu actuel
-            voisin = lieu
-            while voisin is lieu:
-                voisin = tb_lieux[random.randint(0, len(tb_lieux) - 1)]
+            nom_voisin = nom_lieu
+            while nom_voisin is nom_lieu:
+                nom_voisin = random.choice(list(dic_lieux.keys()))
 
-            lieu.tb_id_routes.append(id_route)
-            voisin.tb_id_routes.append(id_route)
+            dic_lieux[nom_lieu].tb_id_routes.append(str(id_route))
+            dic_lieux[nom_voisin].tb_id_routes.append(str(id_route))
 
-            dic_routes[id_route] = route
-            print("Nouvelle route n°" + str(id_route) + " de " + lieu.nom_lieu + " à " + voisin.nom_lieu)
+            dic_routes[str(id_route)] = route
+            print("Nouvelle route n°" + str(id_route) + " de " + str(nom_lieu) + " à " + str(nom_voisin))
 
             id_route += 1
 
-            pourcentage = random.randint(0, 100)
+            pourcentage = numpy.random.randint(0, 100)
 
     # TODO : Parcours du plan, si tous les noeuds ne sont PAS atteignables, on recommence
 
-    return tb_lieux, dic_routes
+    return index_dépot, dic_lieux, dic_routes
 
 
 # ■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■
@@ -355,7 +398,7 @@ import json
 
 def fctGénérerJSON():
     lieux_JSON = []
-    for lieu in tb_lieux:
+    for lieu in dic_lieux.values():
         lieux_JSON.append({
             'nom_lieu': lieu.nom_lieu,
             'type_lieu': str(lieu.type_lieu),
@@ -435,7 +478,7 @@ def fctGénérerGraph():
 
     # Ajout des noeuds au graphe
     nodes_color_map = []
-    for lieu in tb_lieux:
+    for lieu in dic_lieux.values():
         G.add_node(lieu.nom_lieu)
 
         if lieu.type_lieu == TypeLieu.DEPOT:
@@ -444,9 +487,9 @@ def fctGénérerGraph():
             nodes_color_map.append(1)
 
     # Ajout des arêtes au graphe
-    for lieu in tb_lieux:
+    for lieu in dic_lieux.values():
 
-        for voisin in tb_lieux:
+        for voisin in dic_lieux.values():
             if lieu is voisin:
                 continue
 
@@ -501,13 +544,13 @@ def fctGénérerGraph():
 
 # ■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■
 
-tb_lieux = []
+# tb_lieux = []
 dic_routes = {}
 tb_colis = []
 
-tb_lieux, dic_routes = fctGénérerPlan(20)
+index_dépot, dic_lieux, dic_routes = fctGénérerPlan(20)
 
-tb_colis = fctGénérerColis(tb_lieux, 100)
+tb_colis = fctGénérerColis(dic_lieux, 20)
 
 fctGénérerJSON()
 
@@ -515,4 +558,114 @@ fctGénérerGraph()
 
 # ■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■
 
-dic_voisins = fctListeVoisins(tb_lieux)
+dic_voisins = fctListeVoisins(dic_lieux)
+
+
+def fctGénérerTournée(_tb_lieux, _index_dépot, _tb_colis, _type_colis):
+    tb_colis_a_livrer = []
+
+    # compl llinéaire
+    for colis in _tb_colis:
+        if colis.type_colis is _type_colis:
+            tb_colis_a_livrer.append(colis)
+
+    lieu_actuel = _tb_lieux[_index_dépot]
+
+    # chercher a minimiser l'ordre
+
+
+# print(dic_voisins)
+
+# ■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■
+
+def djikstra(_tb_lieux, _dic_voisins, _tb_lieux_livraisons, _index_dépot):
+    lieu_actuel = _tb_lieux[_index_dépot]
+    tb_lieux_visité = []
+
+    itinéraire_trouvé = False
+
+    while not itinéraire_trouvé:
+        tb_lieux_visité.append(lieu_actuel)
+
+        # Recherche de la route la plus rapide parmis les voisins
+
+
+# ■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■
+
+"""
+def astar(_lieu_destination):
+
+    # heure de départ
+    heure_actuelle = tb_lieux[index_dépot].heure_début
+
+    open_list = []
+    closed_list = []
+
+    open_list.append(tb_lieux[index_dépot])
+
+    # Loop until you find the end
+    while len(open_list) > 0:
+
+        # Get the current node
+        current_node = open_list[0]
+        current_index = 0
+        for index, item in enumerate(open_list):
+            if item.f < current_node.f:
+                current_node = item
+                current_index = index
+
+        # Pop current off open list, add to closed list
+        open_list.pop(current_index)
+        closed_list.append(current_node)
+
+        # Found the goal
+        if current_node is _lieu_destination:
+            path = []
+            current = current_node
+            while current is not None:
+                path.append(current.position)
+                current = current.parent
+            return path[::-1]  # Return reversed path
+
+        # Generate children
+        children = []
+        for new_position in [(0, -1), (0, 1), (-1, 0), (1, 0), (-1, -1), (-1, 1), (1, -1),
+                                 (1, 1)]:  # Adjacent squares
+
+            # Get node position
+            node_position = (current_node.position[0] + new_position[0], current_node.position[1] + new_position[1])
+
+            # Make sure within range
+            if node_position[0] > (len(maze) - 1) or node_position[0] < 0 or node_position[1] > (
+                        len(maze[len(maze) - 1]) - 1) or node_position[1] < 0:
+                continue
+
+            # Make sure walkable terrain
+            if maze[node_position[0]][node_position[1]] != 0:
+                continue
+
+            # Create new node
+            new_node = Node(current_node, node_position)
+
+            # Append
+            children.append(new_node)
+
+
+"""
+
+"""
+GENETIQUE
+while len(tb_lieux_livraison) > 0:
+v = 0
+vmin = 0
+c = tb_lieux_livraison[0]
+fmin = 0  # le sous fitness
+
+v += 1
+
+# while v < nb_véhicules:
+
+# liste des lieux a déservir
+
+# déterminer les chemin pour se rendre a chaque lieux
+"""
